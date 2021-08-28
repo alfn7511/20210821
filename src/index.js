c33feebb2a0afda2e2c825495922b30a36386077
kiminhye <alfn7511@gmail.com>
2021-08-28 11:38:14 +0900

    state 관련

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import App from './App'
 
-ReactDom.render(<div>Hello React111</div>, document.getElementById('app'))
+ReactDom.render(<App />, document.getElementById('app'))

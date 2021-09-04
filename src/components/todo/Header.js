8d1d609f16df1e460228aaf0d64e9c6957d2e30c
kiminhye <alfn7511@gmail.com>
2021-09-04 10:40:16 +0900

    header styled add

--- src/components/todo/Header.js
+++ src/components/todo/Header.js
@@ -1,7 +1,47 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div``
+const CheckButton = styled.div`
+  width: 60px;
+  font-size: 0;
+  position: absolute;
+  top: 0;
+  left: -32px;
+  transform: rotate(90deg);
+  height: 65px;
+  width: 65px;
+  &::before {
+    content: '❯';
+    font-size: 22px;
+    color: #e6e6e6;
+    padding: 10px 27px 10px 27px;
+    ${({ checked }) => checked && `color: #737373;`}
+  }
+`
+const Input = styled.input`
+  padding: 16px 16px 16px 60px;
+  border: none;
+  background: rgba(0, 0, 0, 0.003);
+  box-shadow: inset 0 -2px 1px rgb(0 0 0 / 3%);
+  position: relative;
+  margin: 0;
+  width: 100%;
+  font-size: 24px;
+  font-family: inherit;
+  font-weight: inherit;
+  line-height: 1.4em;
+  color: inherit;
+  box-sizing: border-box;
+`
 
 function Header() {
-  return <div>Header</div>
+  return (
+    <Container>
+      <CheckButton a={10} b={20} checked={true} />
+      <Input />
+    </Container>
+  )
 }
 
 export default Header
--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,14 +1,25 @@
 import React from 'react'
+
+import styled from 'styled-components'
+
 import Header from '../components/todo/Header'
 import List from '../components/todo/List'
 import Footer from '../components/todo/Footer'
+
+const Container = styled.div`
+  background: #fff;
+  margin: 130px 0 40px 0;
+  position: relative;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 25px 50px 0 rgba(0, 0, 0, 0.1);
+`
+
 function TodoPage() {
   return (
-    <div>
+    <Container>
       <Header />
       <List />
       <Footer />
-    </div>
+    </Container>
   )
 }
 
